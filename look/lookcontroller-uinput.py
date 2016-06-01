55e013eecbbdbe6694724c2d914a181875c9e684
Jonathan Sanderson <jonathan@quernstone.com>
2016-06-01 11:26:52 +0100

    Ugh. Python

--- look/lookcontroller-uinput.py
+++ look/lookcontroller-uinput.py
@@ -10,7 +10,7 @@ from time import sleep
 import uinput
 
 def arduino_map(x, in_min, in_max, out_min, out_max):
-    return (x - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
+    return  (x - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
 
 def look():	
     deadzone_x = 200
@@ -36,10 +36,10 @@ def look():
             
             x_mapped = arduino_map(x, -1024, 1024, -20, 20)
             y_mapped = arduino_map(y, -1024, 1024, -20, 20)
-            mouse.emit(uinput.REL_X, x_mapped, syn=False)
-            mouse.emit(uinput.REL_Y, y_mapped, syn=False)
-            
-            print(x, y, x_mapped, y_mapped)
+            #~ mouse.emit(uinput.REL_X, x_mapped, syn=False)
+            #~ mouse.emit(uinput.REL_Y, y_mapped, syn=False)
+            #~ 
+            #~ print(x_mapped, y_mapped)
 
             # if int(x) < (0 - deadzone_x) :
             # if int(x) > deadzone_x:
