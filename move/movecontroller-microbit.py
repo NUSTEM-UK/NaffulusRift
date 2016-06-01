72e1db7dffefb977f1ca670716c14509217126e2
Jonathan Sanderson <jonathan.sanderson@northumbria.ac.uk>
2016-06-01 10:11:29 +0100

    Initial Look stubs
    Copied across from Move.

--- move/movecontroller-microbit.py
+++ move/movecontroller-microbit.py
@@ -1,11 +1,24 @@
 from microbit import *
 
+deadzone_x = 200
+deadzone_y = 200
+
 def get_sensor_data():
     x, y, z = accelerometer.get_x(), accelerometer.get_y(), accelerometer.get_z()
     a, b = button_a.was_pressed(), button_b.was_pressed()
     print(x, y, z, a, b)
+    return x, y
 
 while True:
     sleep(100)
-    get_sensor_data()
-    display.show("2", wait=False)
+    x, y = get_sensor_data()
+    if int(x) < ( 0 - deadzone_x ):
+        display.show("A", wait=False)
+	if int(x) > deadzone_x:
+		display.show("D", wait=False)
+	if int(y) < (0 - deadzone_y):
+        display.show("W", wait=False)
+	if int(y) > deadzone_y:
+        display.show("S", wait=False)
+	else:
+		display.show(" ", wait=False)
