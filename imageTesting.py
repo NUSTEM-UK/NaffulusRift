03eab42823ce7701a34344dcedce701f889bfe2b
llewmihs <joeshimwell@gmail.com>
2016-06-06 22:28:49 +0000

    Cleaned up the code for Piculus Rift integration

--- imageTesting.py
+++
@@ -1,5 +0,0 @@
-#import picamera
-import os
-import sys
-#camera = picamera.PiCamera()
-print(sys.argv[1],sys.argv[2])
--- test.py
+++
@@ -1,9 +0,0 @@
-test = []
-x = 1
-y = 2
-
-test.append((x,y, 5, 'sheep'))
-
-a,b,c,d = test[0]
-
-print(a,b,c,d)
